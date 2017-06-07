f3cc741d6db6bfb954d719b435954a7c7eaee9f5
denney <695493589@qq.com>
2017-06-07 22:52:05 +0800

    改一个文件

--- src/main/java/io/github/jhipster/sample/web/rest/AccountResource.java
+++ src/main/java/io/github/jhipster/sample/web/rest/AccountResource.java
@@ -35,7 +35,7 @@ import java.util.*;
 @RestController
 @RequestMapping("/api")
 public class AccountResource {
-//    、、、、
+
 
     private final Logger log = LoggerFactory.getLogger(AccountResource.class);
 
