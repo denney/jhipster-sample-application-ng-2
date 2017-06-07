de4e4bbdf216c6eac02efda7eaa6c6ff4eaf9715
denney <695493589@qq.com>
2017-06-07 23:59:37 +0800

    Signed-off-by: denney <695493589@qq.com>

--- src/main/java/io/github/jhipster/sample/web/rest/AccountResource.java
+++ src/main/java/io/github/jhipster/sample/web/rest/AccountResource.java
@@ -37,7 +37,7 @@ import java.util.*;
 public class AccountResource {
 
 
-          /*【【【【【【【【【【【【*/
+          /*【【【【【【【33333【【【【【*/
     private final Logger log = LoggerFactory.getLogger(AccountResource.class);
 
     private final UserRepository userRepository;
